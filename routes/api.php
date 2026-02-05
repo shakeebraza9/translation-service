87cc76abdd0ab1470bca8f842a06f1ecc189762c
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2026-02-05 17:56:40 +0500

    first commit

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,20 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TranslationController;
 
-Route::prefix('translations')->group(function () {
-    Route::post('/', [TranslationController::class, 'store']);
-    Route::put('{translation}', [TranslationController::class, 'update']);
-    Route::get('/', [TranslationController::class, 'index']);
-    Route::get('/search', [TranslationController::class, 'search']);
-    Route::get('/export/json', [TranslationController::class, 'export']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::prefix('translations')->group(function () {
+        Route::post('/', [TranslationController::class, 'store']);
+        Route::put('{translation}', [TranslationController::class, 'update']);
+        Route::get('/', [TranslationController::class, 'index']);
+        Route::get('/search', [TranslationController::class, 'search']);
+    });
 });
+Route::get('/translations/export/json', [TranslationController::class, 'export']);
